37f0c5c88415134a7edeb3f303ab86b9b7720411
Evan Martin <evan.martin@gmail.com>
2025-03-15 07:46:17 -0700

    subdir

--- whodis/src/main.rs
+++ whodis/src/main.rs
@@ -1,10 +1,8 @@
-use whodis_r::effect::instr_effects;
-
 fn main() {
-    let instrs = whodis_r::decode::sample();
+    let instrs = whodis::decode::sample();
     for instr in instrs {
         println!("{}", instr);
-        let eff = instr_effects(&instr);
+        let eff = whodis::effect::instr_effects(&instr);
         for e in eff {
             let js = serde_json::to_string(&e).unwrap();
             println!("  {}", js);
